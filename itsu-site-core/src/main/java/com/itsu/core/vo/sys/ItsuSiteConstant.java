f605b2dbfa7d96739e326e98a7d7cf901c6faef7
苏犇 <suben@formssi.com>
2021-07-16 16:26:21 +0800

    添加banner version

---
+++ itsu-site-core/src/main/java/com/itsu/core/vo/sys/ItsuSiteConstant.java
@@ -0,0 +1,21 @@
+package com.itsu.core.vo.sys;
+
+/**
+ * @author Jerry.Su
+ * @Date 2021/7/16 11:11
+ */
+public interface ItsuSiteConstant {
+
+    String PLATFORM_NAME = "itsu-site";
+
+    String PLATFORM_VERSION = "1.0.0.RELEASE";
+
+    String PLATFORM_BANNER = "\nooooo ooooooooooooo  .oooooo..o ooooo     ooo          .oooooo..o ooooo ooooooooooooo oooooooooooo \n" +
+            "`888' 8'   888   `8 d8P'    `Y8 `888'     `8'         d8P'    `Y8 `888' 8'   888   `8 `888'     `8 \n" +
+            " 888       888      Y88bo.       888       8          Y88bo.       888       888       888         \n" +
+            " 888       888       `\"Y8888o.   888       8           `\"Y8888o.   888       888       888oooo8    \n" +
+            " 888       888           `\"Y88b  888       8  8888888      `\"Y88b  888       888       888    \"    \n" +
+            " 888       888      oo     .d8P  `88.    .8'          oo     .d8P  888       888       888       o \n" +
+            "o888o     o888o     8\"\"88888P'     `YbodP'            8\"\"88888P'  o888o     o888o     o888ooooood8 \n" +
+            "                                                                            version: " + PLATFORM_VERSION + "\n";
+}
